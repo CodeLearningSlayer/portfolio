d87907af15f420581e7feb96923ce28458e97fc9
CodeLearningSlayer <rubtsov.maks2015@yandex.ru>
2022-09-04 19:03:41 +0300

    added preloader

--- src/js/app.js
+++ src/js/app.js
@@ -4,107 +4,127 @@ import {langObj} from "./modules/languages.js";
 import {changeLang} from "./modules/changeLang.js";
 import removeOnScroll from "./modules/removeHeaderOnScroll.js";
 
-let swiper;
+window.addEventListener("DOMContentLoaded", ()=>{
 
+    let preloader = document.querySelector(".preloader");
 
-function enableSwiper(){
-    swiper = new Swiper('.swiper', {
-        modules:[Navigation],
-        watchSlidesProgress:true,
-        centeredSlides: true,
-        // loop: true,
-        // loopedSlides: 4,
-        // slidesPerGroup:1,
-        navigation: {
-            nextEl: '.swiper-button-next',
-            prevEl: '.swiper-button-prev',
-        },
-        breakpoints:{
-            0:{
-                watchOverflow:true,
-                loop:false,
-            },
-            576:{
-                setWrapperSize:true,
-                slidesPerView: 1,
-            },
-        }
-    });
-}
-
-const breakpoint = window.matchMedia('(max-width: 576px)');
-const breakpointChecker = function() {
-    if (breakpoint.matches == true && swiper!=undefined){
-        swiper.destroy(true, true);
-    }
-    else if (breakpoint.matches==false){
-        return enableSwiper();
-    }
-};
-breakpoint.addListener(breakpointChecker);
-breakpointChecker();
-
-const getId = (link) => link.getAttribute('href').replace('#','');
-
-const observer = new IntersectionObserver((entries)=>{
-    entries.forEach((entry)=>{
-        if (entry.isIntersecting){
-            document.querySelectorAll(".header__nav-link").forEach((link)=>{
-                link.classList.toggle("header__nav-link--active",
-                getId(link)===entry.target.id);
-            });
-        }
+    const closePreloader = new Promise(resolve => {
+        setTimeout(()=> {
+            preloader.classList.add("preloader--hide");
+            resolve();
+        }, 2000);
+    }); 
         
-    })
-},{
-    threshold:0.2,
-});
+    closePreloader.then(()=>{
+        setTimeout(() => {
+            preloader.remove();
 
-function Scroll(event, name){
-    if (event.target.classList.contains(name)) {
-        event.preventDefault();
-        const id = getId(event.target);
-        window.scrollTo({
-            top: document.getElementById(id).offsetTop,
-            behavior:"smooth"
+        }, 600);
+    });
+    
+    let swiper;
+
+    function enableSwiper(){
+        swiper = new Swiper('.swiper', {
+            modules:[Navigation],
+            watchSlidesProgress:true,
+            centeredSlides: true,
+            // loop: true,
+            // loopedSlides: 4,
+            // slidesPerGroup:1,
+            navigation: {
+                nextEl: '.swiper-button-next',
+                prevEl: '.swiper-button-prev',
+            },
+            breakpoints:{
+                0:{
+                    watchOverflow:true,
+                    loop:false,
+                },
+                576:{
+                    setWrapperSize:true,
+                    slidesPerView: 1,
+                },
+            }
         });
     }
-}
+    
+    const breakpoint = window.matchMedia('(max-width: 576px)');
+    const breakpointChecker = function() {
+        if (breakpoint.matches == true && swiper!=undefined){
+            swiper.destroy(true, true);
+        }
+        else if (breakpoint.matches==false){
+            return enableSwiper();
+        }
+    };
+    breakpoint.addListener(breakpointChecker);
+    breakpointChecker();
+    
+    const getId = (link) => link.getAttribute('href').replace('#','');
+    
+    const observer = new IntersectionObserver((entries)=>{
+        entries.forEach((entry)=>{
+            if (entry.isIntersecting){
+                document.querySelectorAll(".header__nav-link").forEach((link)=>{
+                    link.classList.toggle("header__nav-link--active",
+                    getId(link)===entry.target.id);
+                });
+            }
+            
+        })
+    },{
+        threshold:0.2,
+    });
+    
+    function Scroll(event, name){
+        if (event.target.classList.contains(name)) {
+            event.preventDefault();
+            const id = getId(event.target);
+            window.scrollTo({
+                top: document.getElementById(id).offsetTop,
+                behavior:"smooth"
+            });
+        }
+    }
+    
+    
+    let burgerBtn = document.querySelector(".header__burger");
+    burgerBtn.addEventListener("click", function(){
+        document.querySelector(".header__nav").classList.toggle("header__nav--active");
+        burgerBtn.classList.toggle("header__burger--active");
+    });
+    
+    
+    window.addEventListener("scroll", function(){
+        let header = document.querySelector(".header__top");
+        header.classList.toggle("header__top--sticky", window.scrollY > 0);
+    });
+    
+    let upBtn = document.querySelector(".footer__up");
+    let contactBtn = document.querySelector(".content__contact-btn");
+    
+    contactBtn.addEventListener("click", (contact)=> Scroll(contact, "content__contact-btn"));
+    
+    upBtn.addEventListener("click", (home)=>Scroll(home, "footer__up"));
+    
+    
+    
+    document.querySelectorAll(".section").forEach((section)=>observer.observe(section));
+    document.querySelector('.header__nav').addEventListener('click', (event)=> Scroll(event, 'header__nav-link'));
+    document.querySelector('.footer__nav').addEventListener('click', (event)=> Scroll(event, 'footer__nav-link'));
+    
+    
+    //Lang change
+    
+    changeLang();
+    
+    //Removing sticky header on scroll
+    
+    removeOnScroll();
+    
+    
+    // flsFunctions.testWebP();
 
-
-let burgerBtn = document.querySelector(".header__burger");
-burgerBtn.addEventListener("click", function(){
-    document.querySelector(".header__nav").classList.toggle("header__nav--active");
-    burgerBtn.classList.toggle("header__burger--active");
 });
 
-
-window.addEventListener("scroll", function(){
-    let header = document.querySelector(".header__top");
-    header.classList.toggle("header__top--sticky", window.scrollY > 0);
-});
-
-let upBtn = document.querySelector(".footer__up");
-let contactBtn = document.querySelector(".content__contact-btn");
-
-contactBtn.addEventListener("click", (contact)=> Scroll(contact, "content__contact-btn"));
-
-upBtn.addEventListener("click", (home)=>Scroll(home, "footer__up"));
-
-
-
-document.querySelectorAll(".section").forEach((section)=>observer.observe(section));
-document.querySelector('.header__nav').addEventListener('click', (event)=> Scroll(event, 'header__nav-link'));
-document.querySelector('.footer__nav').addEventListener('click', (event)=> Scroll(event, 'footer__nav-link'));
-
-
-//Lang change
-
-changeLang();
-
-//Removing sticky header on scroll
-
-removeOnScroll();
-
-
-// flsFunctions.testWebP();
